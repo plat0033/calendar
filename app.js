ccc0a9ddcc8e166c0db7415aea1175889f8e8f09
plat0033 <plat0033@algonquinlive.com>
2020-09-21 09:54:43 -0400

    profe test

--- app.js
+++ app.js
@@ -1,284 +1,105 @@
-const currentDay= new Date().getDate();
-const currentMonth= new Date().getMonth();
-const currentYear= new Date().getFullYear();
-const currentMonthFirstDay= (new Date(currentYear, currentMonth)).getDay();
-const monthsName= [
-    {month: 'January', value: 1 },
-    {month: 'February', value: 2 },
-    {month: 'March', value: 3 },
-    {month: 'April', value: 4 },
-    {month: 'May', value: 5 },
-    {month: 'June', value: 6 },
-    {month: 'July', value: 7 },
-    {month: 'August', value: 8 },
-    {month: 'September', value: 9 },
-    {month: 'October', value: 10 },
-    {month: 'November', value: 11 },
-    {month: 'December', value: 12 }
-  ];
-  let control_padre = 0;
-
-
-
-Vue.component('simple-button', {
-    props:['month','day','year', 'actual_title', 'actual_hour', 'actual_desc'],
-    data: function(){
-        return {
-            hour: this.actual_hour,
-            title: this.actual_title,
-            description: this.actual_desc,
-            sliderStatus: false
-        }
+Vue.component('day', {
+  props: ['year', 'month', 'day', 'events'],
+  computed: {
+    date: function () {
+      return new Date(this.year, this.month - 1, this.day)
     },
-    methods: {
-        action: function () {
-            //this.title = "titulo2";
-            //console.log(this.title);
-            const event_id= "" + this.month + this.day + this.year 
-            console.log(event_id)
-            const event_detail = JSON.stringify({ title: this.title, detail: this.description, hour: this.hour })
-            localStorage.setItem(event_id, event_detail)
-
-            //localStorage.setItem('')
-            const data = localStorage.getItem(event_id)
-            console.log(data)
-            const obj = JSON.parse(data)
-            console.log(obj.title + obj.detail)
-            this.sliderStatus = false;
-
-
-        },
-
-        clear: function () {
-            const event_id= "" + this.month + this.day + this.year;
-            localStorage.removeItem(event_id);
-            this.sliderStatus = false;
-            
-
-
-        }
-
-       
+    isToday: function () {
+      // let today = new Date();
+      return this.onDate(new Date())
+      // console.log("isToday")
+      // console.log(this.date.getDate() === today.getDate())
+      // return this.date.getDate() === today.getDate()
+    }
+  },
+  methods: {
+    onDate: function (someDate) {
+      return this.date.getDate() === someDate.getDate() &&
+        this.date.getMonth() === someDate.getMonth() &&
+        this.date.getFullYear() === someDate.getFullYear()
     },
-    template: `
-    <div>
-        <div v-if="actual_title != ''">
-            <button class="btn btn-sm btn-outline-success" @click="sliderStatus = true">
-                {{this.title}}-{{this.hour}}
-            </button>
-        </div>
-        <div v-else>
-            <button class="btn btn-sm btn-outline-success" @click="sliderStatus = true">
-                +
-            </button>
-        </div>
-        
-        
-        <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
-                <div v-if="sliderStatus" class="bg-event w-100 h-100">
-                    <div class="w-50 h-50 mx-auto mt-5 eventSheet ">
-                        <button type="button" class="close" @click="sliderStatus = false">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        <div class="row m-3">
-                            <span></span>
-                        </div>
-                        <div class="row m-3">
-                            <h2>Event</h2>
-                        </div>
-                        <div class="row m-0">
-                            <div class="input-group col-6">
-                                <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroup-sizing-default">Title</span>
-                                </div>
-                                <input v-model="title" type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-                            </div>
-                            <div class="input-group col-3"></div>
-                            <div class="input-group col-3">
-                                <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroup-sizing-default">Hour</span>
-                                </div>
-                                <input v-model="hour" type="time" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-                            </div>
-                        </div>
-                        
-                        
-                        <div class="row m-3">
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                <span class="input-group-text">Description</span>
-                                </div>
-                                <textarea v-model="description" class="form-control" aria-label="description"></textarea>
-                            </div>
-                        </div>
-                        <div class="row m-3 ">
-                           <!-- <button class="btn btn-sm btn-outline-success" @click="action">Guardar</button>
-                            <button class="btn btn-sm btn-outline-danger ml-5" @click="clear">Borrar</button> -->
-    
-                            <div class="row m-3" v-if="actual_title != ''">
-                                <form @submit.prevent="action">
-                                    <button type="submit" class="btn btn-primary mr-5">Save</button>
-                                </form>
-
-                                <form @submit="clear">
-                                    <button type="submit" class="btn btn-danger ml-5">Delete</button>
-                                </form>
-                            </div>
-                            <div class="row m-3" v-else>
-                                <form @submit="action">
-                                    <button type="submit" class="btn btn-primary mr-5">Save</button>
-                                </form>
-
-                                <form @submit.prevent="clear">
-                                    <button type="submit" class="btn btn-danger ml-5">Delete</button>
-                                </form>
-                                
-                            </div>
-
-
-
-                        </div>
-                    </div>
-                </div>
-        </transition>
-    </div>
-    `
+    getTime: function (date) {
+      const d = new Date(date)
+      return d.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' })
+    },
+    editEvent: function (event) {
+      this.$root.newEvent = event
+      this.$root.editEventForm = true
+      this.$root.showEventForm = true
+    }
+  },
+  template: `
+  <div class="day" :class="{today: isToday}">
+    <span class="day-number">{{ day }}</span>
+    <div class="event" v-for="event in events" v-if="onDate(new Date(event.date))" @click="editEvent(event)">{{ event.text }} <span class="event-time">{{ getTime(event.date) }}</span></div>
+  </div>`
 })
 
-
 const app = new Vue({
-    el: '#app',
-    data: {
-        columns: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", ],
-        monthSelected: currentMonth+1,
-        yearSelected: currentYear,
-        months: monthsName,
-        events:[],
-        events2:[],
-        event:{},
-        control: control_padre,
-        //sliderStatus: false,  se esta ocupando en el component
-
-        //from the component
-
-        
-        firstD: 0 //only as a control  TBDel
+  el: '#app',
+  data: {
+    month: 3,
+    year: 2020,
+    newEvent: {
+      text: '',
+      date: '',
+      details: ''
     },
-    computed:{
-        displayMonth: function () {
-            //test = this.control
-            //console.log(currentDay);
-            
-            if (this.yearSelected >= 2000 && this.yearSelected <=2100){
+    showEventForm: false,
+    editEventForm: false,
+    months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    events: [
+      { text: 'Web Production', date: '2020-02-03T01:30', details: 'Vue Transitions and localStorage' },
+      { text: 'Web Production', date: '2020-02-10T13:30', details: 'Vue CLI' },
+      { text: 'Web Production', date: '2020-02-17T13:30', details: 'No Class' }
+    ]
+  },
+  mounted: function () {
+    console.log(this.events)
+    if (localStorage.getItem('events')) {
+      this.events = JSON.parse(localStorage.getItem('events'))
+      console.log(this.events)
+    }
 
-                firstDay= (new Date(this.yearSelected, this.monthSelected-1)).getDay()
-                daysInMonth = 32 - new Date(this.yearSelected, this.monthSelected-1, 32).getDate();
-                daysInLastMonth = 32 - new Date(this.yearSelected, this.monthSelected-2, 32).getDate();
-                this.firstD = firstDay //TBDel
-                numberOfWeeks = Math.ceil((daysInMonth + firstDay)/7);
-
-                lastDaysInPrevMonth = daysInLastMonth - firstDay;
-                firstDaysInNextMonth = 0;
-                calendarDay = 0;
-                
-                const data = localStorage.getItem(4292020);   //testing
-                console.log(data);
-
-                
-
-                if(localStorage.getItem(this.monthSelected + '29' + currentYear)){
-                    console.log("dentro");
-                }
-
-                console.log(this.events);
-                //if (localStorage.getItem())
-               
-                
-
-                week= [[],[],[],[],[],[]];
-                events2=[];
-
-                for (let i= 0; i < numberOfWeeks; i++) {
-                    if (i === 0){
-                        for (let j=0; j < firstDay; j++){
-                            lastDaysInPrevMonth++
-                            week[i].push("")
-                        }
-                        for(let j = firstDay; j < 7; j++){
-                            calendarDay++
-                            week[i].push(calendarDay)
-                            console.log(this.monthSelected, calendarDay, currentYear)
-                            
-                            if(localStorage.getItem(this.monthSelected + calendarDay.toString() + currentYear)){
-                                console.log("dentro2");
-                                const value = this.monthSelected + calendarDay.toString() + currentYear;
-                                const data = localStorage.getItem(this.monthSelected + calendarDay.toString() + currentYear);
-                                const obj = JSON.parse(data);
-                                console.log(obj.title + obj.hour);
-                                console.log(value);
-
-                                if(this.events.some(event => event.event_date === value)){
-                                    
-                                } else{
-                                    this.events.push({event_date: value, description: obj})
-                                    this.events2[value] = {title: obj.title, hour: obj.hour , desc: obj.detail};
-                                }
-
-                            }
-                            
-                        }
-                    }else if(i < numberOfWeeks-1 ) {
-                        for (let j=0; j < 7; j++) {
-                            calendarDay++
-                            week[i].push(calendarDay)
-
-                            if(localStorage.getItem(this.monthSelected + calendarDay.toString() + currentYear)){
-                                const value = this.monthSelected + calendarDay.toString() + currentYear;
-                                const data = localStorage.getItem(this.monthSelected + calendarDay.toString() + currentYear);
-                                const obj = JSON.parse(data);
-
-                                if(this.events.some(event => event.event_date === value)){
-                                    
-                                } else{
-                                    this.events.push({event_date: value, description: obj})
-                                    this.events2[value] = {title: obj.title, hour: obj.hour , desc: obj.detail};
-                                }
-
-                            }
-
-                        } 
-                    }else{
-                        for (let j=calendarDay; j < daysInMonth; j++) {
-                            calendarDay++
-                            week[i].push(calendarDay)
-
-                            if(localStorage.getItem(this.monthSelected + calendarDay.toString() + currentYear)){
-                                const value = this.monthSelected + calendarDay.toString() + currentYear;
-                                const data = localStorage.getItem(this.monthSelected + calendarDay.toString() + currentYear);
-                                const obj = JSON.parse(data);
-
-                                if(this.events.some(event => event.event_date === value)){
-                                    
-                                } else{
-                                    this.events.push({event_date: value, description: obj})
-                                    this.events2[value] = {title: obj.title, hour: obj.hour , desc: obj.detail};
-                                }
-
-                            }
-                        } 
-                        lastDays = week[i].length
-                        for (let j= 0; j < (7 - lastDays); j++  ) {
-                            firstDaysInNextMonth++
-                            week[i].push("")
-                        }
-                            
-                    }
-                } 
-                return week
-
-            }
-
-                
-        }  
-    } 
-}) 
+    const today = new Date()
+    this.month = today.getMonth() + 1
+    this.year = today.getFullYear()
+  },
+  watch: {
+    events: {
+      handler: function () {
+        localStorage.setItem('events', JSON.stringify(this.events))
+      },
+      deep: true
+    }
+  },
+  computed: {
+    monthName: function () {
+      return this.months[this.month - 1]
+    },
+    daysInMonth: function () {
+      return new Date(this.year, this.month, 0).getDate()
+    },
+    startDay: function () {
+      return new Date(this.year, this.month - 1, 1).getDay()
+    }
+  },
+  methods: {
+    getEventIndex: function (event) {
+      return this.events.findIndex(e => e.text === event.text && e.date === event.date)
+    },
+    addEvent: function () {
+      this.events.push( {...this.newEvent} )
+      this.close()
+    },
+    deleteEvent: function () {
+      this.events.splice(this.getEventIndex(this.newEvent), 1)
+      this.close()
+    },
+    close: function () {
+      this.showEventForm = false
+      this.newEvent = { text: '', date: '', details: '' }
+      this.editEventForm = false
+    }
+  }
+})
---
+++ bkup/README.md
@@ -0,0 +1,55 @@
+# Event Calendar
+
+## Objective
+Use what you have learned about Directives, Components, Transitions, and LocalStorage to create an Event Calendar web application that allow the user to view scheduled events, make new events, and edit and delete existing events. This project will also require working with dates and times, so please review the [Working with Dates](#working-with-dates) section to learn more.
+
+## Requirements
+The following requirements must be met in order to complete the assignment successfully:
+
+1. The calendar must have a monthly view that accurately displays the selected month and year.
+1. The user should be able to choose which month and year to display. 
+1. When the page loads the current month and year should be displayed with the current day highlighted. 
+1. Events should appear on the calendar on the appropriate day list the time and title of the event.
+1. Clicking the event should display additional details about the event including but not limited to: the event title, time and description.
+1. The user should be able to add new event.
+1. The user should be able to edit or delete existing events.
+1. At least one Vue Component should be used. 
+1. At least on Vue Transition should be used.
+1. All event data should be stored to localStorage. 
+1. The web application should have an appealing and intuitive user experience.
+
+## Working with Dates
+
+As a calendar web application, naturally the project will require working with dates and times. As such, you will need to use of the [JavaScript Date](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date) object or a JavaScript Library like [moment.js](https://momentjs.com). This is not a topic that will be directly covered in this class, but below you will find a list of tutorials and resources that will help.
+
+- **YouTube:** [Working with Time and Dates in JavaScript](https://www.youtube.com/watch?v=weA9FTMzK-E)
+- **Article:** [Understanding Date and Time in JavaScript](https://www.digitalocean.com/community/tutorials/understanding-date-and-time-in-javascript)
+- **Article:** [Everything You Need to Know About Date in JavaScript](https://css-tricks.com/everything-you-need-to-know-about-date-in-javascript/) 
+
+## Example
+
+*Coming Soon*
+
+## Rubric
+
+The following rubric will be use to grade this assignment.
+
+| Task | Points |
+| ---  | :---:  | 
+| Accurately displays monthly calendar | 5 |
+| Successfully allows users to change the currently displayed month and year | 5 |
+| Accurately displays events on monthly calendar | 5 |
+| Successfully allows users to add events | 5 |
+| Successfully allows users to edit and delete existing events | 5 |
+| Uses at least one vue component | 5 |
+| Saves event data to localstorage | 5 |
+| Implements at least one vue transition | 5 | 
+| Provides an appealing and intuitive user experience | 10 | 
+| **Total** | **50** | 
+
+## Submission
+When the project is completed, follow the steps below to submit for grading:
+
+1. Create a commit with the message "Completes the Event Calendar assignment"
+2. Push all commits to GitHub.
+3. Submit the URL to your GitHub repository to the **Event Calendar** assignment on BrightSpace. 
